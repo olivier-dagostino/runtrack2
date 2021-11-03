a8a8ca2bd90976efaef9896985af137192a8409b
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2021-11-03 13:46:53 +0100

    jour02, job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,11 @@
+<?php   
+
+for ($i=0; $i <= 1337 ; $i++) { 
+    echo $i."<br>";
+
+    if ($i == 42) {
+       echo "<u><b>$i</b></u><br>";
+    }
+}
+
+// Pour la valeur ($i=0)qui démarre de 0 et qui va jusqu'à 1337 ($i <= 1337)
