cf766f5eff750ed5aba552be1e4796a351db1756
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2021-11-07 18:57:45 +0100

    jour 4 job01

---
+++ jour04/job01/index.php
@@ -0,0 +1,29 @@
+
+<?php
+
+
+$i = 0;  // commence à 0
+
+foreach ($_GET as $key => $value) {     // 
+    echo $key . " : " . $value."<br>";
+    $i++;
+}
+
+echo "Il y a " . $i . " d'arguments GET";
+
+
+ ?>
+ <!DOCTYPE html>
+ <html lang="en">
+   <head>
+     <meta char-set="utf-8">
+     <title>TEST PHP</title>
+   </head>
+   <body>
+     <form action="" method="get">
+       <input type="text" value="Le Php me prend la tête" name="myfield">
+       <input type="text" value="Le Php me prend le choux" name="myfield2">
+       <input type="submit" value = "post">
+     </form>
+   </body>
+ </html>
