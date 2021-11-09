f07b4c5748a61b179fac7f36748de27a939e88ed
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2021-11-09 15:52:21 +0100

    jour07 job01 et 02

---
+++ jour07/job01/index.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+    
+
+    if(isset($_POST['reset']))
+    {
+        unset ($_SESSION['nbvisites']);
+    }
+
+    if(isset($_SESSION['nbvisites']))
+    {
+        $_SESSION['nbvisites']++;
+    } 
+  
+ 
+    else    
+    {
+        $_SESSION['nbvisites']=0;
+    }
+
+   
+
+    echo $_SESSION['nbvisites'];
+
+?> 
+        
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+<input type="submit" value="reset" name="reset">
+
+    </form>
+</body>
+</html>
