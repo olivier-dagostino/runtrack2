3a5ae4b15cb17865a89a250297af9bef9e5f9f42
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2021-11-10 09:02:22 +0100

    Update index.php

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -1,5 +1,12 @@
 <?php
 
+if (isset($_POST['reset'])) 
+{
+    unset($_COOKIE['nbvisites']);
+    header ("Location: index.php");
+}
+
+
 if (isset($_COOKIE['nbvisites'])) {
     $i = $_COOKIE['nbvisites']+1;
 
@@ -13,7 +20,21 @@ setcookie('nbvisites',$i);
 
 echo $_COOKIE['nbvisites'];
 
-
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="#" method="post">
+        <input type="submit" value="reset" name="reset">
+    </form>
+</body>
+</html>
+
