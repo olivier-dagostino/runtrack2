64233dcf17d40db408003fd13b2712d3f7e38777
Olivier D'AGOSTINO <olivier.d-agostino@laplateforme.io>
2021-11-10 08:47:24 +0100

    fin du job 2

--- jour07/job02/index.php
+++ jour07/job02/index.php
@@ -1,32 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-
-
 <?php
 
-
-if(!isset($_COOKIE['nbvisites'])){
-$value = 0;
-}
-
-else{
-$value=$_COOKIE['nbvisites'] ;
+if (isset($_COOKIE['nbvisites'])) {
+    $i = $_COOKIE['nbvisites']+1;
 
 }
-$value++;
-setcookie('nbvisites', $value);
+
+else { 
+    $i = 0;
+}
+
+setcookie('nbvisites',$i);
+
 echo $_COOKIE['nbvisites'];
 
-?>
 
-<form action="#" method="post">
-<input type="submit" value="reset" name="reset">
-</body>
-</html>
+
+
+?>
